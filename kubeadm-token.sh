965d27f27e6a1bf6297bad70dfc87359cb1cc94c
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-02-08 15:42:58 +0000

    Remove stale script

--- kubeadm-token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-generate_random() {
-  TOKEN=`head -c 32 /dev/urandom | base64 | tr -c [a-zA-Z0-9] -d | head -c $1`
-  echo $TOKEN
-}
-
-echo "$(generate_random 6).$(generate_random 16)"
