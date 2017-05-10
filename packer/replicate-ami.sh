971acc1cd2e8b6a3e2b005c250b506e093684dc7
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-05-10 12:36:14 +0100

    AMI attributes are not preserved on copy, the copies need to be made public

--- packer/replicate-ami.sh
+++ packer/replicate-ami.sh
@@ -27,8 +27,17 @@ readonly regions=(
 new_images="$(jq -n ".\"${source_region}\"={ImageId:\"${source_image}\"}")"
 
 for destination_region in "${regions[@]}" ; do
-  new_image="$(aws ec2 copy-image --source-image-id "${source_image}" --source-region "${source_region}" --region ap-northeast-1 --name "${name}")"
+  echo "Copying to ${destination_region}..."
+  new_image="$(aws ec2 copy-image --source-image-id "${source_image}" --source-region "${source_region}" --region "${destination_region}" --name "${name}")"
   new_images="$(echo "${new_images}" | jq ".\"${destination_region}\"=${new_image}")"
+done
+
+for destination_region in "${regions[@]}" ; do
+  new_image_id="$(echo "${new_images}" | jq -r ".\"${destination_region}\".ImageId")"
+  echo "Waiting for new image (${new_image_id}) to become available..."
+  while test "$(aws ec2 describe-images --region "${destination_region}" --image-ids "${new_image_id}" | jq -r '.Images[0].State')" == "pending" ; do sleep 5 ; done
+  aws ec2 modify-image-attribute --region "${destination_region}" --image-id "${new_image_id}" --launch-permission "{\"Add\": [{\"Group\":\"all\"}]}"
+  echo "Image ${new_image_id} in ${destination_region} is now public."
 done
 
 echo "${new_images}"
